bdff6f0999e67389dc25bf8bab7ff450140f544c
chj2788 <chj2788@gmail.com>
2021-02-03 19:27:24 +0900

    initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyBrG1qqF4rCtZ7cmZQt-fx4EYBhKajNSN8",
+  authDomain: "chat-web-app-49b27.firebaseapp.com",
+  databaseURL: "https://chat-web-app-49b27-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-49b27",
+  storageBucket: "chat-web-app-49b27.appspot.com",
+  messagingSenderId: "264771875147",
+  appId: "1:264771875147:web:cb582861dfd700ef5a78ce",
+};
+
+const app = firebase.initializeApp(config);
