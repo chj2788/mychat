99da1b60c6d22aa8afc90155f5752e48563ead12
chj2788 <chj2788@gmail.com>
2021-02-04 16:19:27 +0900

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Switch } from "react-router";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute>
+        <Home path="/" />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
